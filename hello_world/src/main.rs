4c5c0a1cb4569d28a9dc8c02fcc400ebcbf3ef82
xiaocanmeng <2585609923@qq.com>
2024-03-05 17:51:25 +0900

    add

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let a = [4,3,2,1];
+
+    // 通过索引和值的方式迭代数组 `a` 
+    for (i,v) in a.iter().enumerate() {
+        println!("第{}个元素是{}",i+1,v);
+    }
 }
--- hello_world/target/debug/hello_world.d
+++ hello_world/target/debug/hello_world.d
@@ -1,1 +1,1 @@
-/home/yongman/projects/Rust_learn/hello_world/target/debug/hello_world: /home/yongman/projects/Rust_learn/hello_world/src/main.rs
+/home/dev/work/github/Rust_learn/hello_world/target/debug/hello_world: /home/dev/work/github/Rust_learn/hello_world/src/main.rs
